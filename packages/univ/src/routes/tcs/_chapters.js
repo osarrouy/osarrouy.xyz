e5a48d1216b6528b4b0e8d3c24601a5c1cc5865b
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2020-10-13 17:04:30 +0200

    Add TCS sequence 1

--- packages/univ/src/routes/tcs/_chapters.js
+++ packages/univ/src/routes/tcs/_chapters.js
@@ -1,3 +1,4 @@
 import { metadata as metadata_0 } from './chapters/0.svx'
+import { metadata as metadata_1 } from './chapters/1.svx'
 
-export default [metadata_0]
+export default [metadata_0, metadata_1]
