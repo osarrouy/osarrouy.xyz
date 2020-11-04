42681c99dea16d3090b3de730e6e7a2179b3d465
Olivier Sarrouy <olivier.sarrouy@gmail.com>
2020-11-04 17:30:25 +0100

    Add ICN sequence 3

--- packages/univ/src/routes/icn/_chapters.js
+++ packages/univ/src/routes/icn/_chapters.js
@@ -1,4 +1,5 @@
 import { metadata as metadata_0 } from './chapters/0.svx'
 import { metadata as metadata_1 } from './chapters/1.svx'
+import { metadata as metadata_2 } from './chapters/2.svx'
 
-export default [metadata_0, metadata_1]
+export default [metadata_0, metadata_1, metadata_2]
